c67846fac2c3ff5ad468c5210edd86fe0bdf5d5d
Chay Auriane <chaya.07@protonmail.com>
2022-07-27 15:36:09 +0200

    feat: update view
    #5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel</title>
-</head>
-
-<body>
+<x-guest-layout>
     <header>
         <a href="{{route('home')}}">Accueil</a>
     </header>
     <main>
         @foreach ($articles as $article)
-        <a href="{{route('article', ['id' => $article->id])}}">{{ $article->title }}</a>
-        <p>Nombre de commentaire(s) : {{count($article->comments)}}</p>
+        <a href="{{route('article', ['article' => $article])}}">{{ $article->title }}</a>
+        <p>{{$article->published_at->diffForHumans()}}</p>
+        <p>Nombre de commentaire(s) : {{$article->comments_count}}</p>
         @php
-            $trimedText = Str::limit($article->content, 500);
-            $shortText = substr($trimedText, 0, strrpos($trimedText, ' ')) . '...';
+        $trimedText = Str::limit($article->content, 500);
+        $shortText = substr($trimedText, 0, strrpos($trimedText, ' ')) . '...';
         @endphp
         <p>{{ $shortText }}</p>
-        
+
         @endforeach
         {{ $articles->links() }}
     </main>
-</body>
-
-</html>
+</x-guest-layout>
