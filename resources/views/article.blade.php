8f256c9518e844a118df5eea18bb14c3ce1cbe8d
Chay Auriane <chaya.07@protonmail.com>
2022-07-29 09:58:26 +0200

    refactor: views

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,41 +1,47 @@
 <x-guest-layout>
-    <header>
-        <a href="{{route('home')}}">Accueil</a>
-    </header>
     <main>
-        <h1>{{$article->title}}</h1>
-        <p>{{ $article->published_at->diffForHumans() }}</p>
-        <p>{{$article->user->name}}</p>
-        <p>Commentaire(s) : {{$article->comments_count}}</p>
-        <p>{{$article->content}}</p>
-        <h2>Commentaires</h2>
-        @foreach ($article->comments as $comment)
-        @if ($comment->pseudo !== null)
-        <p>{{$comment->pseudo}}</p>
-        @else
-        <p>{{$comment->user->name}}</p>
+        @if (session()->has('success'))
+            <div class="bg-green-900 text-white">
+                {{ session()->get('success') }}
+            </div>
         @endif
-        <p>{{ $comment->created_at->diffForHumans() }}</p>
-        <p>{{$comment->content}}</p>
+        <h1 class="font-extrabold text-2xl">{{ $article->title }}</h1>
+        <div class="grid grid-cols-3 gap-4 justify-items-center w-full">
+            <p>{{ $article->published_at->diffForHumans() }}</p>
+            <p>{{ $article->user->name }}</p>
+            <p>Commentaire(s) : {{ $article->comments_count }}</p>
+        </div>
+        <p>{{ $article->content }}</p>
+        <h2 class="text-2xl my-5">Commentaires</h2>
+        @foreach ($article->comments as $comment)
+            <div class="my-10">
+                @if ($comment->pseudo !== null)
+                    <p class="font-bold">{{ $comment->pseudo }}</p>
+                @else
+                    <p class=font-bold>{{ $comment->user->name }}</p>
+                @endif
+                <p>{{ $comment->created_at->diffForHumans() }}</p>
+                <p>{{ $comment->content }}</p>
+            </div>
         @endforeach
         <div>
 
-            <h3>Ajouter un commentaire</h3>
-            <form action="{{route('create.comment')}}" method="POST">
+            <h3 class="text-2xl my-5">Ajouter un commentaire</h3>
+            <form action="{{ route('comments.create') }}" method="POST">
                 @csrf
-                <input type="hidden" value="{{$article->id}}" name="articleId">
-                @if(Auth::check())
-                <p>Name : {{Auth::user()->name}}</p>
-                @else
-                <div>
-                    <label>Pseudo</label>
-                    <input type="text" name="pseudo" />
-                </div>
-                <div>
-                    <label>email</label>
-                    <input type="text" name="email" />
-                </div>
-                @endif
+                <input type="hidden" value="{{ $article->id }}" name="articleId">
+                @auth
+                    <p>Name : {{ Auth::user()->name }}</p>
+                    @elseguest
+                    <div>
+                        <label>Pseudo</label>
+                        <input type="text" name="pseudo" />
+                    </div>
+                    <div>
+                        <label>email</label>
+                        <input type="text" name="email" />
+                    </div>
+                @endauth
                 <div>
                     <label>Commentaire</label>
                     <textarea name="content" id="comment-content" cols="30" rows="10" maxlength="2000"></textarea>
@@ -47,13 +53,13 @@
                 </div>
             </form>
             @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
             @endif
         </div>
     </main>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,18 @@
 <x-guest-layout>
-    <header>
-        <a href="{{route('home')}}">Accueil</a>
-    </header>
-    <main>
-        @foreach ($articles as $article)
-        <a href="{{route('article', ['article' => $article])}}">{{ $article->title }}</a>
-        <p>{{$article->published_at->diffForHumans()}}</p>
-        <p>Nombre de commentaire(s) : {{$article->comments_count}}</p>
-        @php
-        $trimedText = Str::limit($article->content, 500);
-        $shortText = substr($trimedText, 0, strrpos($trimedText, ' ')) . '...';
-        @endphp
-        <p>{{ $shortText }}</p>
 
+    <main class="bg-grey-500">
+        @foreach ($articles as $article)
+            <div class="bg-white p-2">
+                <a class="font-bold text-xl"
+                    href="{{ route('articles.show', ['article' => $article]) }}">{{ $article->title }}</a>
+                <p>{{ $article->published_at->diffForHumans() }}</p>
+                <p>Nombre de commentaire(s) : {{ $article->comments_count }}</p>
+                @php
+                    $trimedText = Str::limit($article->content, 500);
+                    $shortText = substr($trimedText, 0, strrpos($trimedText, ' ')) . '...';
+                @endphp
+                <p>{{ $shortText }}</p>
+            </div>
         @endforeach
         {{ $articles->links() }}
     </main>
